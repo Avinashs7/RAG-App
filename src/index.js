3abc0c155ef6f9915137dd0981a16d2001cdfbdb
Avinashs7 <07avinash.s@gmail.com>
2024-08-24 12:42:30 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ const express = require('express')
 const app = express()
 const helmet = require('helmet')
 
+const documentRouter = require('./routes/document.route.js')
 
 app.use(helmet())
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
+app.use("/document", documentRouter)
 
 
 app.listen(8000, () => {
